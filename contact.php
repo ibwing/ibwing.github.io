11d5996a0e270d9fbb961144fd84b64d1de4d287
ibwing <wingate.ivy@gmail.com>
2016-11-13 14:34:34 -0500

    added html

--- contact.php
+++ contact.php
@@ -1,3 +1,68 @@
+<!DOCTYPE html>
+
+<head>
+  <title>Ivy Wingate</title>
+  <style>
+    body {
+      text-align: center;
+      background: black;
+      background-size: cover;
+      background-position: center;
+      color: white;
+      font-family: helvetica;
+    }
+    p {
+      font-size: 22px;
+    }
+    input {
+      border: 0;
+      padding: 10px;
+      font-size: 18px;
+    }
+    input[type="submit"] {
+      background: red;
+      color: white;
+    }
+img {
+	margin: 40px 0px 0px 0px;
+	border: 7px solid white;
+	border-radius: 20px;
+  </style>
+</head>
+<body>
+  <img src="http://i.imgur.com/wEQiABg.jpg?1" alt="Ivy">
+  <p>Hi! I'm Ivy, an Atlanta-based Computer Programmer. Say hello!</p>
+ 
+ 
+<!-- include your own success html here -->
+ 
+<form action="contact.php" method="post">
+<p><b>Your Name:</b> <input type="text" name="yourname" /><br><br>
+<b>Subject:</b> <input type="text" name="subject" /><br><br>
+<b>E-mail:</b> <input type="text" name="email" /><br><br>
+<b>Website:</b> <input type="text" name="website"></p><br><br>
+<p>Do you like this website?
+<input type="radio" name="likeit" value="Yes" checked="checked" /> Yes
+<input type="radio" name="likeit" value="No" /> No
+<input type="radio" name="likeit" value="Not sure" /> Not sure</p>
+<p>How did you find us?
+<select name="how">
+<option value=""> -- Please select -- </option>
+<option>Google</option>
+<option>Yahoo</option>
+<option>Link from a website</option>
+<option>Word of mouth</option>
+<option>Other</option>
+</select>
+<p><b>Your comments:</b><br />
+<textarea name="comments" rows="10" cols="40"></textarea></p>
+<p><input type="submit" value="Send it!"></p>
+</form>
+ 
+ 
+<!--Thank you for contacting us. We will be in touch with you very soon.-->
+</body>
+</html>
 <?php
 /* Set e-mail recipient */
 $myemail  = "wingate.ivy@gmail.com";
